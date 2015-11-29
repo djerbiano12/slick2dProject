d1422058970c30b96cd456970cd6b37af11e8596
Tim Kraut <Tim.Kraut@web.de>
2015-11-29 15:59:49 +0100

    Rendre les animations plus propres

--- slick/src/animation/AnimationPerso.java
+++ slick/src/animation/AnimationPerso.java
@@ -4,33 +4,27 @@ import org.newdawn.slick.Animation;
 import org.newdawn.slick.SlickException;
 
 public class AnimationPerso {
-	private static  AnimationPerso instance;
-	
+    private static AnimationPerso instance;
+    private static Animation[]    animation;
+    private static final int NOMBRE_ETATS_ANIMATION = 8;
+    private static final int TUILE_LARGEUR          = 64;
+    private static final int TUILE_HAUTEUR          = 64;
 
-	private static Animation[] animation;
-	
+    public AnimationPerso() throws SlickException {
+        super();
 
-	public AnimationPerso() throws SlickException {
-		super();
-		animation=OutilsAnimation.initAnimation(8,"map/tuiles/perso.png", 64, 64);
-	}
+        animation = OutilsAnimation.initAnimation(NOMBRE_ETATS_ANIMATION, "map/tuiles/perso.png", TUILE_LARGEUR, TUILE_HAUTEUR);
+    }
 
+    public static Animation[] getInstance() {
+        if (instance == null) {
+            try {
+                instance = new AnimationPerso();
+            } catch (SlickException e) {
+                e.printStackTrace();
+            }
+        }
 
-
-	public static Animation[] getInstance()  {
-  
-            if (instance == null)
-				try {
-					instance = new AnimationPerso();
-				} catch (SlickException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-           
-            return animation;
-	}
-
-
-	
-	
+        return animation;
+    }
 }
--- slick/src/animation/OutilsAnimation.java
+++ slick/src/animation/OutilsAnimation.java
@@ -5,39 +5,38 @@ import org.newdawn.slick.SlickException;
 import org.newdawn.slick.SpriteSheet;
 
 public final class OutilsAnimation {
+    public static final int HAUT   = 0;
+    public static final int GAUCHE = 1;
+    public static final int BAS    = 2;
+    public static final int DROITE = 3;
 
+    public static Animation[] initAnimation(int nombreEtats, String fichier, int tuileLargeur, int tuileHauteur) throws SlickException {
+        Animation[] animations  = new Animation[nombreEtats];
+        SpriteSheet spriteSheet = new SpriteSheet(fichier, tuileLargeur, tuileHauteur);
 
+        // sans mouvement
+        animations[0] = loadAnimation(spriteSheet, 0, 1, 0);
+        animations[1] = loadAnimation(spriteSheet, 0, 1, 1);
+        animations[2] = loadAnimation(spriteSheet, 0, 1, 2);
+        animations[3] = loadAnimation(spriteSheet, 0, 1, 3);
 
-	public static final int HAUT = 0;
-	public static final int GAUCHE = 1;
-	public static final int BAS = 2;
-	public static final int DROITE = 3;
+        // animations
+        animations[4] = loadAnimation(spriteSheet, 1, 9, 0);
+        animations[5] = loadAnimation(spriteSheet, 1, 9, 1);
+        animations[6] = loadAnimation(spriteSheet, 1, 9, 2);
+        animations[7] = loadAnimation(spriteSheet, 1, 9, 3);
 
-	
-	
-	public static Animation[] initAnimation(int n, String fichier, int tuile_w, int tuile_h) throws SlickException {
-		Animation[] animations = new Animation[n];
-		SpriteSheet spriteSheet = new SpriteSheet("map/tuiles/perso.png",tuile_w,tuile_h);
-		// sans mouvement
-		animations[0] = loadAnimation(spriteSheet, 0, 1, 0);
-		animations[1] = loadAnimation(spriteSheet, 0, 1, 1);
-		animations[2] = loadAnimation(spriteSheet, 0, 1, 2);
-		animations[3] = loadAnimation(spriteSheet, 0, 1, 3);
-		// animations
-		animations[4] = loadAnimation(spriteSheet, 1, 9, 0);
-		animations[5] = loadAnimation(spriteSheet, 1, 9, 1);
-		animations[6] = loadAnimation(spriteSheet, 1, 9, 2);
-		animations[7] = loadAnimation(spriteSheet, 1, 9, 3);
-		
-		return animations;
-	}
-	
-	
-	private static Animation loadAnimation(SpriteSheet spriteSheet, int startX, int endX, int y) {
-		Animation animation = new Animation();
-		for (int x = startX; x < endX; x++) {
-			animation.addFrame(spriteSheet.getSprite(x, y), 100);
-		}
-		return animation;
-	}
+        return animations;
+    }
+
+    private static Animation loadAnimation(SpriteSheet spriteSheet, int startX, int endX, int y) {
+        final int ANIMATION_DURATION_MS = 100;
+        Animation animation             = new Animation();
+
+        for (int x = startX; x < endX; x++) {
+            animation.addFrame(spriteSheet.getSprite(x, y), ANIMATION_DURATION_MS);
+        }
+
+        return animation;
+    }
 }
