2f85f2eb5c03c058b5d1c0bbc04f9e3acd6c071d
aminebms4@gmail.com <aminebms4@gmail.com>
2015-12-02 12:48:31 +0100

    Modifications Diverses !

--- slick/src/model/Piece.java
+++ slick/src/model/Piece.java
@@ -5,8 +5,9 @@ import org.newdawn.slick.Graphics;
 
 public class Piece extends ElementJeuStatique{
 
+	public static final String IMAGE_PIECE = "ressources/piece.png";
 	public Piece(int positionX, int positionY) {
-		super(positionX, positionY,32,32, "ressources/piece.png");
+		super(positionX, positionY,32,32, Piece.IMAGE_PIECE);
 	}
 	//////////////////////////
 	/// Mettre dans la vue 
